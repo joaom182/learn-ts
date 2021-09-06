c617db10378fdbd0bfc4f2f7b752ac5f021adb63
joaom182 <joaom182@hotmail.com>
2021-09-06 00:35:37 -0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { sum } from '~src/sum';
+
+function run() {
+  console.log(sum('5'));
+}
+
+run();
+
+export {};
---
+++ src/sum.ts
@@ -0,0 +1,3 @@
+export function sum(value: string) {
+  return parseFloat(value) + 2;
+}
